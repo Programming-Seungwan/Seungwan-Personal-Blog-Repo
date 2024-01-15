0a0b93082688cfa0288d112f5cac2e3f842a5add
Programming-Seungwan <swsj4480@gmail.com>
2024-01-15 16:42:11 +0900

    Feat: make 404 error page
    1. 사용자가 예상치 못한 경로로 들어오면 이는 404.js 로 이어지게 됨
    2. 사용자에게 404 에러 UI를 보여주고 10초가 지나면 redirect를 해줌
    3. 해당 페이지의 스타일링 정도만 styled-components로 해주었음

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,49 +1,94 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { navigate } from 'gatsby';
+import { faBan } from '@fortawesome/free-solid-svg-icons';
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const ErrorContainer = styled.div`
+  width: 100dvw;
+  height: 100dvh;
+  background-color: var(--background-color1);
+  display: flex;
+  flex-direction: column;
+`;
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
+const ErrorMessageContainer = styled.div`
+  flex-grow: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  span {
+    font-size: 1.7rem;
+    font-weight: 600;
+  }
+`;
+
+const ErrorLogoContainer = styled.div`
+  flex-grow: 4;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+
+  span {
+    font-size: 1rem;
+    font-weight: 600;
+  }
+`;
+
+const RedirectButtonContainer = styled.div`
+  flex-grow: 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  button {
+    border: none;
+    background: none;
+    padding: none;
+    font: inherit;
+    cursor: pointer;
+    width: 30%;
+    height: 40%;
+    font-size: 1.5rem;
+    font-weight: 550;
+    border-radius: 0.5rem;
+    color: white;
+    background-color: rgba(0, 0, 0, 1);
+  }
+`;
+
+function handleClickGoHomtButtons() {
+  navigate('/');
 }
 
 const NotFoundPage = () => {
+  React.useEffect(() => {
+    const timer = setTimeout(() => navigate('/'), 10000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <ErrorContainer>
+      <ErrorMessageContainer>
+        <span>404 Error! Page Not Found 😥</span>
+      </ErrorMessageContainer>
 
-export default NotFoundPage
+      <ErrorLogoContainer>
+        <FontAwesomeIcon icon={faBan} size='10x' />
+        <span>This page is redirected after 10 seconds ...</span>
+      </ErrorLogoContainer>
 
-export const Head = () => <title>Not found</title>
+      <RedirectButtonContainer>
+        <button onClick={handleClickGoHomtButtons}>Go Home</button>
+      </RedirectButtonContainer>
+    </ErrorContainer>
+  );
+};
+
+export default NotFoundPage;
+
+export const Head = () => <title>Not found</title>;
