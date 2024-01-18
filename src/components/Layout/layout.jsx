d4655e07725712c3fc37e5a46408a91589fe33a0
Programming-Seungwan <swsj4480@gmail.com>
2024-01-18 17:43:46 +0900

    Fix : 모바일 브라우저에서 하단 바를 가리는 문제 해결 시도
    1. Layout 컴포넌트에서 useEffect 훅으로 계산하여 적용

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { container } from './layout.module.css';
 import Navbar from '../navigation/navbar.jsx';
 import { useRecoilState } from 'recoil';
@@ -14,8 +14,33 @@ const Layout = ({ pageTitle, children }) => {
       return;
     }
   }
+  // 브라우저의 높이를 해결하기 위한 useEffect
+  const [windowHeight, setWindowHeight] = useState(0);
+  useEffect(() => {
+    // 윈도우 높이를 설정하는 함수
+    const handleResize = () => {
+      setWindowHeight(window.innerHeight);
+    };
+
+    // 컴포넌트가 마운트되었을 때 초기 윈도우 높이를 설정
+    handleResize();
+
+    // 윈도우 크기가 변경될 때마다 높이를 업데이트
+    window.addEventListener('resize', handleResize);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const style = {
+    height: `${windowHeight}px`,
+    // 다른 스타일들
+  };
+
   return (
-    <main className={container} onClick={handleClickContainer}>
+    <main className={container} onClick={handleClickContainer} style={style}>
       <Navbar />
       {children}
     </main>
