bad9a49f2319a08473fc773cf774a76cca4bda52
Programming-Seungwan <swsj4480@gmail.com>
2024-01-29 23:54:02 +0900

    🐞Fix : 쓸데 없는 브라우저 높이관련 이슈 코드 제거

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -14,33 +14,33 @@ const Layout = ({ pageTitle, children }) => {
       return;
     }
   }
-  // 브라우저의 높이를 해결하기 위한 useEffect
-  const [windowHeight, setWindowHeight] = useState(0);
-  useEffect(() => {
-    // 윈도우 높이를 설정하는 함수
-    const handleResize = () => {
-      setWindowHeight(window.innerHeight);
-    };
+  // // 브라우저의 높이를 해결하기 위한 useEffect
+  // const [windowHeight, setWindowHeight] = useState(0);
+  // useEffect(() => {
+  //   // 윈도우 높이를 설정하는 함수
+  //   const handleResize = () => {
+  //     setWindowHeight(window.innerHeight);
+  //   };
 
-    // 컴포넌트가 마운트되었을 때 초기 윈도우 높이를 설정
-    handleResize();
+  //   // 컴포넌트가 마운트되었을 때 초기 윈도우 높이를 설정
+  //   handleResize();
 
-    // 윈도우 크기가 변경될 때마다 높이를 업데이트
-    window.addEventListener('resize', handleResize);
+  //   // 윈도우 크기가 변경될 때마다 높이를 업데이트
+  //   window.addEventListener('resize', handleResize);
 
-    // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  //   // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   };
+  // }, []);
 
-  const style = {
-    height: `${windowHeight}px`,
-    // 다른 스타일들
-  };
+  // const style = {
+  //   height: `${windowHeight}px`,
+  //   // 다른 스타일들
+  // };
 
   return (
-    <main className={container} onClick={handleClickContainer} style={style}>
+    <main className={container} onClick={handleClickContainer}>
       <Navbar />
       {children}
     </main>
