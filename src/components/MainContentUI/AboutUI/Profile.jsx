d6a9612098270824c1610592b2bc3a122a764fb0
Programming-Seungwan <swsj4480@gmail.com>
2024-01-29 22:52:22 +0900

    Feat: make /about page toggle button

--- src/components/MainContentUI/AboutUI/Profile.jsx
+++ src/components/MainContentUI/AboutUI/Profile.jsx
@@ -3,53 +3,70 @@ import { StaticImage } from 'gatsby-plugin-image';
 import {
   profileContainer,
   profileImage,
+  profileToggleContainer,
   lifeCareer,
   lifeTitle,
   schoolCareer,
   schoolCareerItem,
   logo,
 } from './index.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function Profile() {
+  const [profileToggle, setProfileToggle] = React.useState(true);
+
+  function handleClickProfileToggleButton() {
+    setProfileToggle((before) => !before);
+  }
   return (
     <div className={profileContainer}>
       <div>
         <StaticImage src='./seungwan.jpg' alt='seungwan photo' className={profileImage} />
       </div>
 
-      <div className={lifeCareer}>
-        <div className={lifeTitle}>Seungwan, Kim</div>
-        <div className={schoolCareer}>
-          <div className={schoolCareerItem}>
-            <div>
-              <p>Whimoon High School 109th</p>
-              <h4>from 2014 ~ 2016</h4>
+      <div className={lifeTitle}>Seungwan, Kim</div>
+
+      <div className={profileToggleContainer}>
+        {profileToggle && <FontAwesomeIcon icon={faCaretRight} size='2x' onClick={handleClickProfileToggleButton} />}
+        {!profileToggle && <FontAwesomeIcon icon={faCaretDown} size='2x' onClick={handleClickProfileToggleButton} />}
+        <span>Career</span>
+      </div>
+
+      {!profileToggle && (
+        <div className={lifeCareer}>
+          <div className={schoolCareer}>
+            <div className={schoolCareerItem}>
+              <div>
+                <p>Whimoon High School 109th</p>
+                <h4>from 2014 ~ 2016</h4>
+              </div>
+              <StaticImage src='./whimoon.webp' alt='whimoon logo' className={logo} />
             </div>
-            <StaticImage src='./whimoon.webp' alt='whimoon logo' className={logo} />
-          </div>
-          <div className={schoolCareerItem}>
-            <div>
-              <p>Hongik Business Administration</p>
-              <h4>from 2018 ~ </h4>
+            <div className={schoolCareerItem}>
+              <div>
+                <p>Hongik Business Administration</p>
+                <h4>from 2018 ~ </h4>
+              </div>
+              <StaticImage src='./hongik.jpg' alt='hongik logo' className={logo} />
             </div>
-            <StaticImage src='./hongik.jpg' alt='hongik logo' className={logo} />
-          </div>
-          <div className={schoolCareerItem}>
-            <div>
-              <p>Military Army</p>
-              <h4>from 2019.03 ~ 2020. 10</h4>
+            <div className={schoolCareerItem}>
+              <div>
+                <p>Military Army</p>
+                <h4>from 2019.03 ~ 2020. 10</h4>
+              </div>
+              <StaticImage src='./junjin.JPG' alt='junjin logo' className={logo} />
             </div>
-            <StaticImage src='./junjin.JPG' alt='junjin logo' className={logo} />
-          </div>
-          <div className={schoolCareerItem}>
-            <div>
-              <p>Hongik Computer Engineering</p>
-              <h4>from 2022 ~</h4>
+            <div className={schoolCareerItem}>
+              <div>
+                <p>Hongik Computer Engineering</p>
+                <h4>from 2022 ~</h4>
+              </div>
+              <StaticImage src='./hongik.jpg' alt='hongik logo' className={logo} />
             </div>
-            <StaticImage src='./hongik.jpg' alt='hongik logo' className={logo} />
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
